c719a898b058a90fbb11a15a6258606c4f47f523
pyromaniac3 <97550706+pyromaniac3@users.noreply.github.com>
2023-04-22 00:53:43 -0700

    updated play and menu
    they have more visual components now

---
+++ src/scenes/Play.js
@@ -0,0 +1,21 @@
+class Play extends Phaser.Scene{
+    constructor(){
+        super("playScene");
+    }
+    preload(){
+        // load images/tile sprites
+        this.load.image('rocket','./assets/rocket.jpeg');
+        this.load.image('ship','./assets/ship.jpeg');
+        this.load.image('space','./assets/space.jpeg');
+    }
+
+    create(){
+        // green UI background
+        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
+        // white borders
+        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+    }
+}
